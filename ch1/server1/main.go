7819a2d7b41fe70461df2200aea21c98342e01ed
shilinlee <836160610@qq.com>
2020-11-01 21:02:50 +0800

    go web server

---
+++ ch1/server1/main.go
@@ -0,0 +1,19 @@
+// go run main.go
+// // 浏览器访问 http://localhost:8000/hello
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
