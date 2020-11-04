ec70434405c22834e68222980299588c99271152
shilinlee <836160610@qq.com>
2020-11-04 23:35:22 +0800

    Package & File

---
+++ ch2/tempconv0/celsius.go
@@ -0,0 +1,26 @@
+// 包tempconv 进行摄氏温度和华氏温度的转换计算
+
+package tempconv
+
+import "fmt"
+
+type Celsius float64
+type Fahrenheit float64
+
+const (
+	AbsoluteZeroC Celsius = -273.15
+	FreezingC     Celsius = 0
+	BoilingC      Celsius = 100
+)
+
+func CToF(c Celsius) Fahrenheit {
+	return Fahrenheit(c*9/5 + 32)
+}
+
+func FToC(f Fahrenheit) Celsius {
+	return Celsius((f - 32) * 5 / 9)
+}
+
+func (c Celsius) String() string {
+	return fmt.Sprintf("%g°C", c)
+}
