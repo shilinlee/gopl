b0cb89e28150788a75bceea71c7aeace042710d0
shilinlee <836160610@qq.com>
2020-12-09 23:39:17 +0800

    文本和HTML模板

---
+++ ch4/autoscape/main.go
@@ -0,0 +1,28 @@
+// go run main.go
+package main
+
+import (
+	"html/template"
+	"log"
+	"os"
+)
+
+//!+
+func main() {
+	const templ = `<p>A: {{.A}}</p><p>B: {{.B}}</p>`
+	t := template.Must(template.New("escape").Parse(templ))
+	var data struct {
+		A string        // 不受信任的纯文本
+		B template.HTML // 受信任的HTML
+	}
+	data.A = "<b>Hello!</b>"
+	data.B = "<b>Hello!</b>"
+	if err := t.Execute(os.Stdout, data); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+/*
+<p>A: &lt;b&gt;Hello!&lt;/b&gt;</p><p>B: <b>Hello!</b></p>
+ */
