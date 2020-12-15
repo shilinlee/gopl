025e4590ee39d5bc2f44dc25c27fc22fc43c1c70
shilinlee <836160610@qq.com>
2020-12-15 23:46:04 +0800

    递归和多返回值

---
+++ ch5/outline/main.go
@@ -0,0 +1,29 @@
+// 在gopl目录下面
+//  go run ch1/fetch/main.go https://github.com | go run ch5/outline/main.go
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/html"
+	"os"
+)
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "outline: %v\n", err)
+		os.Exit(1)
+	}
+	outline(nil, doc)
+}
+
+func outline(stack []string, n *html.Node) {
+	//fmt.Println("----", stack)
+	if n.Type == html.ElementNode {
+		stack = append(stack, n.Data)
+		fmt.Println(stack)
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		outline(stack, c)
+	}
+}
